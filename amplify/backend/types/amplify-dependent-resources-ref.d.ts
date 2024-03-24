c32949c3fd79ad7d54471147380c98462cadcb6d
BT TECHNOLOGY <bttechnology@BTs-MacBook-Pro.local>
2024-03-24 17:46:30 +0300

    Auth changes

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "notesappcf19b670": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
