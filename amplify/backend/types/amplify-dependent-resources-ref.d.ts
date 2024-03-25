e8cae186fda4b90425732b780f04754fca376cdb
BT TECHNOLOGY <bttechnology@BTs-MacBook-Pro.local>
2024-03-25 11:21:35 +0300

    header changes

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s3bucket23": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
